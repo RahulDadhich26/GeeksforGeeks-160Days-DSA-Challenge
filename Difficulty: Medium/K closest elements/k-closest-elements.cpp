1cd144422871a713d4f6597a45159f8660069858
TheGaurdiann <111578507+RahulDadhich26@users.noreply.github.com>
2025-06-12 23:58:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K closest elements/k-closest-elements.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    vector<int> printKClosest(vector<int> arr, int k, int x) {
+        vector<int> ans;
+        priority_queue<pair<int, int> ,vector<pair<int, int>> , greater<pair<int, int>>>pq;
+        for (int i=0; i<arr.size(); i++){
+            if(x!= arr[i]){
+                int ele= arr[i];
+                int putele= abs(ele- x);
+                pq.push(make_pair(putele, -arr[i]));
+            }
+            else{
+                continue;
+            }
+        }
+        
+        while(k--){
+            auto top= pq.top();
+            pq.pop();
+            int ele= top.second;
+            ans.push_back(abs(ele));
+             
+        }
+        return ans;
+    }
+};
