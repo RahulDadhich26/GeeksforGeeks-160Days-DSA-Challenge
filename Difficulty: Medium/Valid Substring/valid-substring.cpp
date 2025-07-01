4f27a015f064e86566734b75f93052640f913b92
TheGaurdiann <111578507+RahulDadhich26@users.noreply.github.com>
2025-07-01 05:49:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Valid Substring/valid-substring.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int maxLen(string& s) {
+          stack<int>st;
+        st.push(-1);
+        int max_len = 0;
+        for(int i =0 ;i<s.length();i++){
+            if(s[i] == '('){
+                st.push(i);
+            }
+            else{
+                st.pop();
+                if(st.empty()){
+                    st.push(i);
+                }
+                else{
+                    int curr_len = i - st.top();
+                    max_len = max(max_len,curr_len);
+                }
+            }
+        }
+        return max_len;
+    }
+};
